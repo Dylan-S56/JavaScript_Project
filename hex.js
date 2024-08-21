7b936c8217afe6b6fd248f52a75f259649a97103
Dylan-S56 <dylan.suren@gmail.com>
2024-08-21 19:12:06 +0100

    Corrected spelling of specific words
    Corrected the spelling of words like "colour" to the British spelling.

--- hex.js
+++ hex.js
@@ -1,18 +1,18 @@
-// Hex color flipper
+// Hex colour flipper
 
-// The hex color flipper selects any hex color at random.
+// The hex colour flipper selects any hex color at random.
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-// The .addEventerListener command lets the user click on the button to trigger the color flipper.
+// The .addEventerListener command lets the user click on the button to trigger the colour flipper.
 btn.addEventListener('click', function(){
     let hexColor = '#';
     for(let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()];
     }
     
-    // When the button is clicked, the background color will automatically change to whatever hex color was picked by the flipper. The exact hex code of said color will also be displayed in text.
+    // When the button is clicked, the background colour will automatically change to whatever hex colour was picked by the flipper. The exact hex code of said colour will also be displayed in text.
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 });
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Simple color flipper
+// Simple colour flipper
 
-// The simple color flipper selects one of the four defined colors at random.
+// The simple colour flipper selects one of the four defined colours at random.
 const colors = ["cyan", "green", "rgba(255,65,0)", "#10006d"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-// The .addEventerListener command lets the user click on the button to trigger the color flipper.
+// The .addEventerListener command lets the user click on the button to trigger the colour flipper.
 btn.addEventListener('click', function(){
     // Get random number between 0 and 3 
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
-// Whenever the button is clicked, the background color of the webpage will change to any of the four colors defined in the colors variable. If the new color ends up being the same as the current one, then the website will stay the same.
+// Whenever the button is clicked, the background colour of the webpage will change to any of the four colours defined in the colours variable. If the new colour ends up being the same as the current one, then the website will stay the same.
     document.body.style.backgroundColor = colors[randomNumber]; color.textContent = colors[randomNumber];
 });
 
